865001d627797a41348e5de773ef7d794e3091b2
Trafire <antoinewood@gmail.com>
2020-08-18 19:34:58 -0400

    update git ignore

---
+++ secrets_settings.py
@@ -0,0 +1,8 @@
+import os
+
+os.environ["DB_HOST"] = 'localhost'
+os.environ["DB_PORT"] = '3306'
+os.environ["DB_NAME"] = 'jobs'
+os.environ["DB_USER"] = 'afg_antoine'
+os.environ["DB_PASSWORD"] = 'gbuuyxMwFroM687K'
+os.environ["SECRET_KEY"] = '(e0c=b$9xkch67c*1+v3ny_b^l_f+y738_(vn!4s!qos@8urk4'
